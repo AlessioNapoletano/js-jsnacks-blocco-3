d49d2f79250f31885531c08cd8f354cb4157b7b8
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-11-28 18:06:04 +0100

    Add Snack 4

--- Esercizio 3/js/script.js
+++ Esercizio 3/js/script.js
@@ -1,17 +1,6 @@
-// Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
-
-
-/*
-    CREA DUE ARRAY CON NUMERI DIVERSI DI ELEMENTI
-    AGGIUNGI ALL'ARRAY CHE HA MENO ELEMENTI TANTI ELEMENTI FINO A CHE NON ARRIVA ALLA STESSA LUNGHZZA DELL'ALTRO
-
-*/
 
 const numberList1 = [1, 2, 3, 4, 5, 6, 7, 8];
 const numberList2 = [1, 2, 3, 4];
-
-
-
 
 while (numberList1.length != numberList2.length) {
     if (numberList1.length > numberList2.length) {
@@ -20,5 +9,6 @@ while (numberList1.length != numberList2.length) {
         numberList1.push(Math.floor(Math.random() * 10));
     }
 }
+
 console.log(numberList1);
 console.log(numberList2);
