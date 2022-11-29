30e06b27ad9ca1973a23a2945e4bf4af23640912
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-11-29 12:56:41 +0100

    modified

--- Esercizio 6/js/script.js
+++ Esercizio 6/js/script.js
@@ -1,20 +1,16 @@
-/**
- *
- * Crea una funzione che dato un array, calcoli la somma dei suoi elementi.
- *
- */
 
-const list = [1, 2, 3, 4, 20, "ciao" , 5 , 6, 10 , "dispari"]
+const list = [1, 2, 3, 4, 20, "ciao", 5, 6, 10, "dispari" , "ciao3"];
 
 function getSumOfArrayElements(array) {
     let sum = 0;
     for (let i = 0; i < array.length; i++) {
-        if(array[i] / array[i] == 1){
-            sum = sum + array[i];
+        const castedElement = parseInt(array[i], 10);
+
+        if (!Number.isNaN(castedElement)) {
+            sum = sum + castedElement;
         }
     }
     return sum;
 }
-
 
 console.log(getSumOfArrayElements(list));
