ef600c33a56a7171a558262d1717153a20d94059
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-11-30 11:39:05 +0100

    Add more style

--- Esercizio 7/js/script.js
+++ Esercizio 7/js/script.js
@@ -17,13 +17,15 @@ function newPElement(array){
         if (array[i] % 2 === 0){
             const divElement = document.getElementById("odd");
             const newPElement = document.createElement("p");
-            newPElement.classList = "font-green";
+            divElement.classList = "d-flex";
+            newPElement.classList.add("font-green", "padding");
             newPElement.innerHTML = array[i];
             divElement.append(newPElement);
         } else {
             const divElement = document.getElementById("even");
             const newPElement = document.createElement("p");
-            newPElement.classList = "font-red";
+            divElement.classList = "d-flex";
+            newPElement.classList.add("font-red", "padding");
             newPElement.innerHTML = array[i];
             divElement.append(newPElement);
         }
