c877f463d5c3f6a110d4cadfb98a7e5452bfb944
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-11-30 11:32:42 +0100

    Create Es7

---
+++ Esercizio 7/js/script.js
@@ -0,0 +1,30 @@
+/**
+ * Crea due tag div con due id diversi.
+ * Un div avrà il testo colorato di rosso mentre l'altro di verde.
+ * Partendo da un array di numeri,
+ * creiamo un nuovo elemento p e lo stampiamo nell'div rosso  se è un numero dispari
+ * e nel div verde se è un numero pari.
+ *
+ *
+ */
+
+const array = [1, 33, 55, 64, 33, 22, 10, 44];
+
+newPElement(array);
+
+function newPElement(array){
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0){
+            const divElement = document.getElementById("odd");
+            const newPElement = document.createElement("p");
+            newPElement.innerHTML = array[i];
+            divElement.append(newPElement);
+        } else {
+            const divElement = document.getElementById("even");
+            const newPElement = document.createElement("p");
+            newPElement.innerHTML = array[i];
+            divElement.append(newPElement);
+        }
+    }
+}
+
